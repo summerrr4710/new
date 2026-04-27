65cd13b0f2c6456d1eb89a8e8b7bcc11cf9d6509
Summer <summer4710@proton.me>
2026-04-28 00:55:03 +0100

    java

---
+++ javaproject/src/com/summer/javaproject/main.java
@@ -0,0 +1,16 @@
+package com.summer.javaproject;
+
+public class main {
+
+
+	public static void main(String[] args) {
+		
+		screwfixCatalogue c = new screwfixCatalogue();
+		
+		System.out.println("hi");
+		System.out.println("");
+	
+		c.list();
+	}
+		
+}
---
+++ javaproject/src/com/summer/javaproject/screwfixCatalogue.java
@@ -0,0 +1,17 @@
+package com.summer.javaproject;
+
+public class screwfixCatalogue {
+
+	public screwfixCatalogue() {
+		
+	}
+	
+	public void list() {
+		System.out.println("save up to 50% on selected power tools");
+		System.out.println("forge steel screwdriver set 12 pcs");
+		System.out.println("save uo to 20% on selected downlights");
+		System.out.println("karcher pressure washers");
+
+	}
+	
+}
