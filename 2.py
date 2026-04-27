20c696ff33c9f61702e10e5b9d65e37f177a914c
Summer <summer4710@proton.me>
2026-04-28 00:16:12 +0100

    2

---
+++ 2.py
@@ -0,0 +1,13 @@
+
+
+print("2")
+
+# Defining main function
+def main():
+    print("hey 22222222222222222222222")
+
+
+# Using the special variable 
+# __name__
+if __name__=="__main__":
+    main()
